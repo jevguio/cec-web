0ca7ca5247f415aa2a59dc87a3aa15654bc3b688
John Vincent S. Guioguio <jvguioguio@gmail.com>
2024-07-28 14:18:13 +0800

    awsd

--- config/database.php
+++ config/database.php
@@ -18,16 +18,7 @@ return [
 
     'default' => env('DB_CONNECTION', 'mysql'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Below are all of the database connections defined for your application.
-    | An example configuration is provided for each database system which
-    | is supported by Laravel. You're free to add / remove connections.
-    |
-    */
+    
 
     'connections' => [
 
