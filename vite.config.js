f5263448e10b95e1bcff1c3c743a63635537a727
John Vincent S. Guioguio <jvguioguio@gmail.com>
2024-07-28 12:30:12 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
+    ],build: {
+        outDir: 'public/dist',
+    },
 });
