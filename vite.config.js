2b16f7a84e23bb8a399c618d75217d2613ee053b
John Vincent S. Guioguio <jvguioguio@gmail.com>
2024-08-12 16:16:48 +0800

    aw

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-refresh';
 
 import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
-        react(),
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/jsx/main.jsx',
+                'resources/js/app.js', 
             ],
             refresh: true,
         }),
