4ba10ea9584c9420b277c76f27530ba885375f52
John Vincent S. Guioguio <jvguioguio@gmail.com>
2024-08-12 16:12:36 +0800

    ga

--- config/database.php
+++ config/database.php
@@ -18,7 +18,16 @@ return [
 
     'default' => env('DB_CONNECTION', 'mysql'),
 
-    
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    |
+    | Below are all of the database connections defined for your application.
+    | An example configuration is provided for each database system which
+    | is supported by Laravel. You're free to add / remove connections.
+    |
+    */
 
     'connections' => [
 
@@ -35,12 +44,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL','mysql://root:IrAmCmupqxUVrzMnVGPWtmRsZmxXjsXn@mysql.railway.internal:3306/railway'),
-            'host' => env('DB_HOST', 'mysql.railway.internal'),
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'railway'),
+            'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'IrAmCmupqxUVrzMnVGPWtmRsZmxXjsXn'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
